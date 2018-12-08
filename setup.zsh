9dbd30318538d68f454404b7a0aa3567c42fa513
A.M <andares@outlook.com>
2018-12-08 20:14:07 +0800

    fix bug

--- setup.zsh
+++ setup.zsh
@@ -32,11 +32,12 @@ main() { # line +4
         local force=1
     }
     if [[ -d "$YHOME" ]] {
-        if [[ force ]] {
+        if [[ $force ]] {
             echo "${YELLOW}Remove previous installed Yup.${NORMAL}"
             rm -rf ~/.yup
         } else {
             echo "${YELLOW}You already have Yup installed.${NORMAL}"
+            echo "Run `yup upgrade` for new version."
             exit
         }
     }
@@ -47,8 +48,14 @@ main() { # line +4
     }
 
     git clone https://github.com/veeshan-io/yup.git $YHOME
-
     git -C $YHOME/.libs clone https://github.com/veeshan-io/ylib.git
+
+    # 构建 yuprc 放入.zshrc中 并执行一次
+    # 执行yup update，下载plug 包括扫描各个plug的bin目录 复制文件进.bin
+    # .autoload生成 包括: 各plug的init.zsh libs的pub部分 运行一遍，并放进yuprc中
+
+    # cmd采用实时扫描 core则是写死
+    # .libs目录下的文件也是实时扫描不预生成
 }
 
 main $*
